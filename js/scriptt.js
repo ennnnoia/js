3418721c7b448aa64f19e83a88f3ec3f4ecfec2a
ennnnoia <120515837+ennnnoia@users.noreply.github.com>
2023-05-24 19:53:21 +0300

    Add files via upload

--- js/scriptt.js
+++ js/scriptt.js
@@ -1,4 +1,4 @@
-//аккордеон
+//АККОРДЕОН
 const faqs = document.querySelectorAll(".que");
 
 faqs.forEach(que => {
@@ -8,7 +8,7 @@ faqs.forEach(que => {
 })
 
 
-//слайдер2
+//СЛАЙДЕР
 // Важно, чтобы было постороено дом дерево и ничего не сломалось
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
@@ -72,15 +72,19 @@ window.addEventListener('DOMContentLoaded', function () {
 
 
 
-// Модальное окно
+//МОДАЛЬНОЕ ОКНО
 const modal = document.getElementById("modal");
 const btn = document.getElementById("modal-btn");
 const closeBtn = document.querySelector(".modal-close");
+const btn1 = document.querySelector("#modall-btn");
 
 btn.addEventListener("click", function () {
     modal.classList.add("modal_active");
 
     closeBtn.addEventListener("click", closeModal);
+
+    btn1.addEventListener("click", closeModal);
+
 
     function closeModal() {
         modal.classList.remove("modal_active");
@@ -100,6 +104,124 @@ btn.addEventListener("click", function () {
     }
 });
 
+
+//модальное окно закрыть
+const modall = document.getElementById("modal-next");
+const btnn = document.getElementById("modall-btn");
+const closeBtnn = document.querySelector(".modal-closee");
+const closeBtnnn = document.querySelector("#closeee");
+
+btnn.addEventListener("click", function () {
+    modall.classList.add("modal-next_active");
+
+    closeBtnn.addEventListener("click", closeModall);
+
+    closeBtnnn.addEventListener("click", closeModall);
+
+
+    function closeModall() {
+        modall.classList.remove("modal-next_active");
+
+        closeBtnn.removeEventListener("click", closeModall);
+
+        closeBtnnn.addEventListener("click", closeModall);
+
+        modall.removeEventListener("click", hideModall);
+    }
+
+    modall.addEventListener("click", hideModall);
+
+    //Закрытие при клике вне зоны модального окна
+    function hideModall(event) {
+        if (event.target === modall) {
+            closeModall();
+        }
+    }
+});
+
+
+//модальное окно фильтрация
+const modalc = document.getElementById("modal-cat");
+const btnnn = document.getElementById("modalo-btn");
+const closeBtNO = document.querySelector(".modal-closoo");
+
+btnnn.addEventListener("click", function () {
+    modalc.classList.add("modal-cat_active");
+
+    closeBtNO.addEventListener("click", closeModalo);
+
+    function closeModalo() {
+        modalc.classList.remove("modal-cat_active");
+
+        closeBtNO.removeEventListener("click", closeModalo);
+
+        modalc.removeEventListener("click", hideModalo);
+    }
+
+    modalc.addEventListener("click", hideModalo);
+
+    //Закрытие при клике вне зоны модального окна
+    function hideModalo(event) {
+        if (event.target === modalc) {
+            closeModalo();
+        }
+    }
+});
+
+
+
+//ФИЛЬТРАЦИЯ
+//Получаем табы (переключатели), родительский элемент табов, и контент табов
+let tab = document.querySelectorAll(".info-header-tab"),
+info = document.querySelector(".info-header"),
+tabContect = document.querySelectorAll(".info-tabcontent");
+//Функция которая скрывает контентные табы
+function hideTabContect(a) {
+for (let i = a; i < tabContect.length; i++) {
+    tabContect[i].classList.remove("show");
+    tabContect[i].classList.add("hide");
+}
+}
+//Скрываются все кроме 1 элемента
+hideTabContect(1);
+//Функция показа контентных табов
+function ShowTabContect(b) {
+if (tabContect[b].classList.contains("hide")) {
+    tabContect[b].classList.remove("hide");
+    tabContect[b].classList.add("show");
+}
+}
+
+//Создаем событие клика на родителя, используя делегирование событий
+info.addEventListener("click", function (event) {
+let target = event.target;
+
+if (target && target.classList.contains("info-header-tab")) {
+    // Связь элементов при совпадении target
+    for (let i = 0; i < tab.length; i++) {
+        //если совпадают
+        if (target == tab[i]) {
+            //Скрываем все табы
+            hideTabContect(0);
+            // удаляем класс активности таба
+            tab.forEach((item) => {
+                item.classList.remove("active");
+            });
+            //элементу target(на который кликнули) добавляем активный класс
+            target.classList.add("active");
+
+            //Совпадение по нумерации
+            ShowTabContect(i);
+
+            break;
+        }
+    }
+}
+});
+
+
+
+//БУРГЕР-МЕНЮ
 
 
 
